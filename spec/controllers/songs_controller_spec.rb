d01e07b30a4eff857db66af1709d45536fd4ecf0
Rudy Ravelin <rudy@noukod.com>
2019-03-26 16:48:42 +0000

    Automatically backed up by Learn

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -1,12 +1,12 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe SongsController do
 #   describe "file upload" do
 #     before do
 #       Song.destroy_all
 #       Artist.destroy_all
 #     end
-# 
+#
 #     it "uploads and processes a file" do
 #       post :upload, file: fixture_file_upload('songs.csv', 'text/csv')
 #       expect(Song.all.count).to eq 13
